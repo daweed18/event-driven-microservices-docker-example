51271a456ec40e5ee8a6338d99a21ec1544f8466
TuNombre <tuemail@example.com>
2025-05-13 10:14:44 -0600

    Initial commit with Auth service

---
+++ app-new.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Auth Service is running!');
+});
+
+app.listen(PORT, () => {
+    console.log('Auth Service listening on port 3000');
+});
+
+
